d15d339a8ffb25c10e0ea4cc7d5cde63837ff70b
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-11 09:41:43 -0300

    Utilizando StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,32 +1,28 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#131016',
-      }}
-    >
-      <Text
-        style={{
-          color: '#FDFCFE',
-          fontSize: 24,
-          fontWeight: 'bold',
-          marginTop: 48,
-        }}
-      >
-        Nome do Evento
-      </Text>
-      <Text
-        style={{
-          color: '#6B6B6B',
-          fontSize: 16,
-          fontWeight: 'bold',
-        }}
-      >
-        Sexta, 11 de Abril de 2023
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Nome do Evento</Text>
+      <Text style={styles.subtitle}>Terça, 11 de Abril de 2023</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+  },
+  title: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  subtitle: {
+    color: '#6B6B6B',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
