b6e8e3dbd206473244915fd7250f9f4f1c0e11fc
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-11 09:11:20 -0300

    Estilizando Elementos

--- App.tsx
+++ App.tsx
@@ -2,9 +2,31 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>React</Text>
-      <Text>Native</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#131016',
+      }}
+    >
+      <Text
+        style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48,
+        }}
+      >
+        Nome do Evento
+      </Text>
+      <Text
+        style={{
+          color: '#6B6B6B',
+          fontSize: 16,
+          fontWeight: 'bold',
+        }}
+      >
+        Sexta, 11 de Abril de 2023
+      </Text>
     </View>
   );
 }
